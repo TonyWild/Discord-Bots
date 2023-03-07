89b08f74093964b2d3fab863fcf978fa17e37c84
TonyWild <sungu0027@naver.com>
2023-03-07 14:45:33 +0900

    feat: 장소별 식당 선택 추가

--- puplebot.py
+++ puplebot.py
@@ -24,14 +24,24 @@ async def on_message(message):
         return
 
     # 퍼플이 인사
-    if message.content == "퍼플이 안녕":
+    if message.content == '퍼플이 안녕':
         await message.channel.send(f"{message.author.mention}도 안녕안녕~")
 
-    # 식당 랜덤 선택
-    if message.content.startswith("!식당"):
-        await message.channel.send(f"어은동에 있는 식당을 무작위로 선택합니다.")
 
-        rest = restaurant.choose_random("어은동 음식점")
+    message_token = message.content.split(' ')
+
+    # 식당 랜덤 선택
+    if message_token[0] == '!식당':
+        # `!식당`
+        if len(message_token) == 1:
+            query = '어은동'
+        # `!식당 궁동`
+        else:
+            query = message_token[1]
+
+        await message.channel.send(f"{query}에 있는 식당을 무작위로 선택합니다.")
+
+        rest = restaurant.choose_random(f'{query} 음식점')
 
         embed=discord.Embed(title="식당 랜덤 선택 결과", color=0x8621ca)
         embed.add_field(name="선택된 식당", value=rest['place_name'], inline=False)
